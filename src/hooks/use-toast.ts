dc33e6358bf01405aaef0433e36d043cbf02c8ac
zafajardo9 <zafajardo9.programming@gmail.com>
2025-01-23 15:42:21 +0800

    Refactor action type definitions in use-toast hook for improved readability
    - Simplified action type definitions by replacing constant references with string literals.
    - Enhanced clarity and consistency in the Action type structure.

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -25,30 +25,21 @@ function genId() {
   return count.toString()
 }
 
-const actionTypes = {
-  ADD_TOAST: "ADD_TOAST",
-  UPDATE_TOAST: "UPDATE_TOAST",
-  DISMISS_TOAST: "DISMISS_TOAST",
-  REMOVE_TOAST: "REMOVE_TOAST",
-} as const
-
-type ActionType = typeof actionTypes
-
 type Action =
   | {
-    type: typeof actionTypes.ADD_TOAST
+    type: "ADD_TOAST"
     toast: ToasterToast
   }
   | {
-    type: typeof actionTypes.UPDATE_TOAST
+    type: "UPDATE_TOAST"
     toast: Partial<ToasterToast>
   }
   | {
-    type: typeof actionTypes.DISMISS_TOAST
+    type: "DISMISS_TOAST"
     toastId?: ToasterToast["id"]
   }
   | {
-    type: typeof actionTypes.REMOVE_TOAST
+    type: "REMOVE_TOAST"
     toastId?: ToasterToast["id"]
   }
 
